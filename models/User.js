67335c58aef91b4733dc4e43b1a57a3cfc605cf8
stpark <dmc0110@naver.com>
2022-06-10 23:44:10 +0900

    메시지

--- models/User.js
+++ models/User.js
@@ -26,4 +26,5 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
+module.exports = {User}
